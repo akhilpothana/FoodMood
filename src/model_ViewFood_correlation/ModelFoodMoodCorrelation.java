941a5c873862a60e455e9f63aa67ee54a85f0b17
edgardoreinoso <edgardoreinoso@67.22.25.166>
2017-09-17 18:55:34 -0400

    Created some more classes in the MVC packages for the Food/Mood Correlation use case. In addition, deleted one package that was identical to the foodmood_v1

---
+++ src/model_ViewFood_correlation/ModelFoodMoodCorrelation.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model_ViewFood_correlation;
+
+/**
+ * This class correspond to the Controller within the MVC structure. This is where the main control of
+ * The functionality will take place. This class will connect both the model with the view
+ * @author edgardoreinoso
+ */
+public class ModelFoodMoodCorrelation {
+    
+}
---
+++ src/view_FoodMood_correlation/ViewFoodMoodCorrelation.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view_FoodMood_correlation;
+
+/**
+ * This class correspond to the Model within the MVC structure. This is where the data of this use case
+ * Functionality will be stored and reused.
+ * @author edgardoreinoso
+ */
+public class ViewFoodMoodCorrelation {
+    
+}
