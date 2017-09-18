1ca0bb73ffcbb6b64745cb40b30384372871949c
Shawn <Shawn@Shawnslaptop>
2017-09-17 20:36:24 -0400

    created Recommendations use case

---
+++ src/RecommendationController/RecommendationController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RecommendationController;
+
+/**
+ *This is the RecommendationController class that manages all processes in the
+ * Recommendations page
+ * 
+ * @author Shawn
+ */
+public class RecommendationController {
+    
+    /**
+     * This is the default constructor for the RecommendationController
+     * class
+     */
+    public RecommendationController(){
+        
+    }
+    
+}
