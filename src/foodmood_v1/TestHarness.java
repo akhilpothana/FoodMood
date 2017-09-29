686245153b73c3a51cbdf4bd6e2ff7e17f323668
edgardoreinoso <edgardoreinoso@67.22.25.166>
2017-09-29 16:29:59 -0400

    Made some changes on the TestHarness class

--- src/foodmood_v1/TestHarness.java
+++ src/foodmood_v1/TestHarness.java
@@ -13,12 +13,18 @@ import CrudFoodController.crud_food_control;
  */
 public class TestHarness {
     //FIELDS
+    //Classes
     private FoodMoodMaxController testAkhilHarness;
     private crud_food_control foodControlHarness;
-    
+    //Other types
     private String food = "dog food";
     
     public TestHarness(){
         foodControlHarness = new crud_food_control(food);
+        TestCRUDFood();
+    }
+    
+    public void TestCRUDFood() {
+        foodControlHarness.getFood();
     }
 }
