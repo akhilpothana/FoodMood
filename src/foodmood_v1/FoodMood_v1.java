964355836d3916a852523f66b2da4a61c2cd65fd
edgardoreinoso <edgardoreinoso@67.22.25.166>
2017-09-29 16:39:04 -0400

    Made some changes on the TestHarness class

--- src/foodmood_v1/FoodMood_v1.java
+++ src/foodmood_v1/FoodMood_v1.java
@@ -5,17 +5,32 @@
  */
 package foodmood_v1;
 
+import CrudFoodController.crud_food_control;
+
 /**
  * This class is the initiator of the application
  * @author Akhil
  */
 public class FoodMood_v1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        TestHarness test = new TestHarness();
+            TestHarness test = new TestHarness();
+    }
+}
+
+class TestHarness {
+    //FIELDS
+    //Classes
+    private FoodMoodMaxController testAkhilHarness;
+    private crud_food_control foodControlHarness;
+    //Other types
+    private String food = "dog food";
+    
+    public TestHarness(){
+        foodControlHarness = new crud_food_control(food);
+        TestCRUDFood();
     }
     
+    public void TestCRUDFood() {
+        foodControlHarness.getFood();
+    }
 }
