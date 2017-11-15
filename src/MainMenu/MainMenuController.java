d03b997b5c818dbde457a9422c006c2d232ae82c
edgardoreinoso <edgardoreinoso@67.22.25.166>
2017-11-15 18:08:27 -0500

    I am trying to pull the new changes

--- src/MainMenu/MainMenuController.java
+++ src/MainMenu/MainMenuController.java
@@ -17,7 +17,7 @@ import javax.swing.JPanel;
 
 public class MainMenuController {
     
-    private MainMenuUI mainMenuUI;
+    private MainMenuUI mainMenuUI;≥
     
     //These will be the only instances of these class and will be 
     //passed around as necessary
